d4f9f46e38af1dee4797864f6c23152c8dbbbde2
7760476962 <86604222+7760476962@users.noreply.github.com>
2021-07-06 08:19:04 +0530

    Add files via upload

---
+++ ArithmaticDemo.java
@@ -0,0 +1,12 @@
+package eg1;
+
+public class ArithmaticDemo {
+	public static void main(String[] args) {
+		int a =12,b=5;
+		System.out.println("a+b="+(a+b));
+		System.out.println("a-b="+(a-b));
+		System.out.println("a*b="+(a*b));
+		System.out.println("a%b="+(a%b));
+	}
+
+}
